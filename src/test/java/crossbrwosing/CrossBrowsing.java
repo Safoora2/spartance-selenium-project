52f4ac0acb8a6706140206cbe53f5c3bc548a94c
Safoora <safooraqasimi2@gmail.com>
2024-07-14 15:27:51 -0400

    Initial Selenium project

---
+++ src/test/java/crossbrwosing/CrossBrowsing.java
@@ -0,0 +1,24 @@
+package crossbrwosing;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CrossBrowsing {
+
+    public static void main(String[] args) {
+
+// open browser in Edge browser
+        EdgeDriver driver = new EdgeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com");
+        String title=driver.getTitle();
+        System.out.println(title);
+        driver.quit();
+
+
+    }
+}
